3028c36425641457f291cf6aebf2d5c01afd7670
Sagar Sangani <sagarsangani29@gmail.com>
2024-06-12 19:06:02 -0400

    Move setupTest

--- test/setupTestEnv.ts
+++ test/setupTestEnv.ts
@@ -1,9 +1,10 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-// Import js-dom extensions for vitest assertions
-import "@testing-library/jest-dom/vitest";
 
 // Reset DOM after each test
 afterEach(() => {
   cleanup();
 });
+
+// Import js-dom extensions for vitest assertions
+import "@testing-library/jest-dom/vitest";
