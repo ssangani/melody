9f96abffd690dcb068e4c0773785fb747f1d63f1
Sagar Sangani <sagarsangani29@gmail.com>
2024-07-05 22:41:47 -0400

    Remove LiveReload from root (#43)
    Use of LiveReload is deprecated when Remix is used with Vite

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -26,7 +25,6 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
